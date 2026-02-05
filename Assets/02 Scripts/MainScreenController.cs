4cab0186146f9bfe17da053062bb1ed510c7031f
김민영 <kmy12083@gmail.com>
2026-02-05 16:11:36 +0900

    main screen 260205
    main screen UI
    added Singleton.cs, GameManager.cs

---
+++ Assets/02 Scripts/MainScreenController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MainScreenController : MonoBehaviour
+{
+    public void OnClickSinglePlayButton()
+    {
+        //TODO : first player button
+    }
+
+    public void OnClickMultiPlayButton()
+    {
+        //TODO : multi player button
+    }
+
+    public void OnClickSettingsButton()
+    {
+        //TODO : settings button
+    }
+}
